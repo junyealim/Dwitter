ecc34805089ef6e5eb7612f286740574a9d43fa3
jun <junyealim@gmail.com>
2023-11-16 14:01:19 +0900

    mongodb 적용

--- data/auth.js
+++ data/auth.js
@@ -1,15 +1,19 @@
-import {db} from '../db/database.js';
-
+import {getUsers} from '../db/database.js';
+import MongoDB from 'mongodb';
+const ObjectID = MongoDB.ObjectId; // unique로 항상 붙던 Id 변수를 하나 만듬
 
 export async function findByUsername(username){
-    return db.execute('SELECT * FROM users WHERE username = ?',[username]).then((result) => result[0][0])
+    return getUsers().find({username}).next().then(mapOptionalUser)
 }
 
 export async function findById(id){
-    return db.execute('SELECT * FROM users WHERE id = ?',[id]).then((result) => result[0][0])
+    return getUsers().find({_id: new ObjectID(id)}).next().then(mapOptionalUser) // 몽고디비에서 사용하는 ObjectID형식으로 id를 변환해서 찾은 후 id값으로 넣어줌
 }
 
 export async function createUser(user) {
-    const {username, password, name, email, url } = user;
-    return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)', [username, password, name, email, url]).then((result)=> result[0].insertId);
+    return getUsers().insertOne(user).then((result) => console.log(result.insertedId.toString()));
+}
+
+function mapOptionalUser(user) {
+    return user ? {...user, id: user._id.toString()} : user;
 }
--- data/tweet.js
+++ data/tweet.js
@@ -1,29 +1,72 @@
-import {db} from '../db/database.js';
+// import {db} from '../db/database.js';
+import MongoDb from 'mongodb';
+import { getTweets, getUsers } from '../db/database.js';
+import * as UserRepository from './auth.js';
+import { Result } from 'express-validator';
+
+const ObjectID = MongoDb.ObjectId;
 
 const SELECT_JOIN = 'SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.email, us.url from tweets as tw JOIN users as us ON tw.userId = us.id';
 
 const ORDER_DESC = 'ORDER BY tw.createdAt DESC'
 
 export async function getAll() {
-    return db.execute(`${SELECT_JOIN} ${ORDER_DESC}`).then((result) => result[0]);
+    return getTweets()
+        .find()
+        .sort({ createdAt: -1 })
+        .toArray()
+        .then(mapTweets);
 }
 
 export async function getAllByUsername(username){
-    return db.execute(`${SELECT_JOIN} WHERE username = ? ${ORDER_DESC}`, [username]).then((result) => result[0]);
+    return getTweets()
+        .find({username})
+        .sort({ createdAt: -1 })
+        .toArray()
+        .then(mapTweets);
 }
 
 export async function getById(id){
-    return db.execute(`${SELECT_JOIN} WHERE tw.id = ? ${ORDER_DESC}`, [id]).then((result) => result[0][0])
+    return getTweets()
+        .find({_id: new ObjectID(id)})
+        .next()
+        .then(mapOptionalTweet);
 }
 
 export async function create(text, userId){
-    return db.execute('INSERT INTO tweets (text, createdAt, userId) VALUES (?, ?, ?)', [text, new Date(), userId]).then((result)=> getById(result[0].insertId));
+    return UserRepository.findById(userId)
+        .then((user) =>
+            getTweets().insertOne({
+                text,
+                createdAt: new Date(),
+                userId,
+                username: user.username,
+                url: user.url
+            })
+            )
+            .then((result) => getById(result.insertedId))
+            .then(mapOptionalTweet);
 }
 
 export async function update(id, text) {
-    return db.execute('UPDATE tweets SET text = ? WHERE id =?', [text, id]).then(()=> getById(id));
+    return getTweets().findOneAndUpdate(
+        {_id: new ObjectID(id)},
+        { $set: {text}},
+        {returnDocument : 'after'}
+    )
+    .then((result) => result)
+    .then(mapOptionalTweet);
 }
 
 export async function remove(id) {
-    return db.execute('DELETE FROM tweets WHERE id = ?', [id]);
+    return getTweets().deleteOne({ _id: new ObjectID(id)})
+}
+
+function mapTweets(tweets) {
+    return tweets.map(mapOptionalTweet)
+}
+
+function mapOptionalTweet(tweet){
+    return tweet ? {...tweet, id: tweet.insertedId}:
+    tweet;
 }
